0951962fb2fd94cfc6abe4b65007a69a235caa7a
limjcst <limjcst@163.com>
2018-09-07 14:06:07 +0800

    Add C# way.
    Signed-off-by: limjcst <limjcst@163.com>

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+/**
+ * This an example to print "Hello World"
+ */
+public class Hello
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hello World");
+    }
+}
